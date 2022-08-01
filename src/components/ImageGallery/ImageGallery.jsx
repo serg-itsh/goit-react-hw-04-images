bb7fb7ecc3947901f849d0e3e21bf0e39529a732
Sergey Shevchenko <sergeishevchenko111@gmail.com>
2022-08-01 22:41:43 +0300

    Ркфакторинг кода на хуки

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
+// import { Component } from 'react';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 import SearchApi from 'components/Searchbar/SearchApi';
 import { Button } from 'components/Button/Button';
@@ -13,59 +14,60 @@ const STATUS = {
   error: 'error',
 };
 
-export class ImageGallery extends Component {
-  state = {
-    gallery: [],
-    totalHits: null,
-    page: 1,
-    status: STATUS.idle,
-  };
+export const ImageGallery = ({ query, handleOpenModal }) => {
+  const [gallery, setGallery] = useState([]);
+  const [totalHits, setTotalHits] = useState(null);
+  const [page, setPage] = useState(1);
+  const [status, setStatus] = useState(STATUS.idle);
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.query !== this.props.query) {
-      this.setState({ status: STATUS.loading });
-      SearchApi(this.props.query)
-        .then(result => {
-          const { data } = result;
-          if (data.hits.length === 0) {
-            alert('You enter invalid search request');
-          }
-          this.setState(prevState => ({
-            gallery: [...data.hits],
-            page: 2,
-            totalHits: data.totalHits,
-            status: STATUS.success,
-          }));
-        })
-        .catch(error => {
-          this.setState({ status: STATUS.error, error });
-        });
+  useEffect(() => {
+    if (query === '') {
+      return;
     }
-  }
-
-  handleLoader = () => {
-    SearchApi(this.props.query, this.state.page)
-      .then(res => {
-        const { hits } = res.data;
-        this.setState(prevState => ({
-          gallery: [...prevState.gallery, ...hits],
-          page: prevState.page + 1,
-        }));
+    setStatus(STATUS.loading);
+    SearchApi(query)
+      .then(result => {
+        const { data } = result;
+        setGallery([...data.hits]);
+        setPage(2);
+        setTotalHits(data.totalHits);
+        setStatus(STATUS.success);
       })
       .catch(error => {
-        this.setState({ status: STATUS.error, error });
+        setStatus(STATUS.error);
+        alert('You enter invalid search request');
+      });
+
+    // if (data.hits.length === 0) {
+    // alert('You enter invalid search request');
+    //     }
+  }, [query]);
+
+  // }
+
+  const handleLoader = () => {
+    SearchApi(query, page)
+      .then(res => {
+        const { hits } = res.data;
+        setGallery(prevState => [...prevState, ...hits]);
+        setPage(page + 1);
+      })
+      .catch(error => {
+        setStatus(STATUS.error);
+        console.log(error);
       });
   };
 
-  render() {
-    const { gallery, totalHits, page, status, error } = this.state;
-    if (status === STATUS.loading) {
-      return <Loader />;
-    }
-    if (status === STATUS.error) {
-      return <p>{error}</p>;
-    }
+  // const { gallery, totalHits, page, status, error } = state;
 
+  if (status === STATUS.loading) {
+    return <Loader />;
+  }
+
+  // if (status === STATUS.error) {
+  //   return <p>{error}</p>;
+  // }
+  if (status === STATUS.success) {
     return (
       <>
         <ul className={styles.ImageGallery}>
@@ -75,18 +77,98 @@ export class ImageGallery extends Component {
                 key={id}
                 smallImg={webformatURL}
                 largeImg={largeImageURL}
-                handleOpenModal={this.props.handleOpenModal}
+                handleOpenModal={handleOpenModal}
               />
             );
           })}
         </ul>
-        {totalHits >= 12 * page && <Button onClick={this.handleLoader} />}
+        {totalHits >= 12 * page && <Button onClick={handleLoader} />}
       </>
     );
   }
-}
+};
 
 ImageGallery.propTypes = {
   handleOpenModal: PropTypes.func.isRequired,
   query: PropTypes.string.isRequired,
 };
+
+//Copi
+
+// export class ImageGallery extends Component {
+//   state = {
+//     gallery: [],
+//     totalHits: null,
+//     page: 1,
+//     status: STATUS.idle,
+//   };
+
+//   componentDidUpdate(prevProps) {
+//     if (prevProps.query !== this.props.query) {
+//       this.setState({ status: STATUS.loading });
+//       SearchApi(this.props.query)
+//         .then(result => {
+//           const { data } = result;
+//           if (data.hits.length === 0) {
+//             alert('You enter invalid search request');
+//           }
+//           this.setState(prevState => ({
+//             gallery: [...data.hits],
+//             page: 2,
+//             totalHits: data.totalHits,
+//             status: STATUS.success,
+//           }));
+//         })
+//         .catch(error => {
+//           this.setState({ status: STATUS.error, error });
+//         });
+//     }
+//   }
+
+//   handleLoader = () => {
+//     SearchApi(this.props.query, this.state.page)
+//       .then(res => {
+//         const { hits } = res.data;
+//         this.setState(prevState => ({
+//           gallery: [...prevState.gallery, ...hits],
+//           page: prevState.page + 1,
+//         }));
+//       })
+//       .catch(error => {
+//         this.setState({ status: STATUS.error, error });
+//       });
+//   };
+
+//   render() {
+//     const { gallery, totalHits, page, status, error } = this.state;
+//     if (status === STATUS.loading) {
+//       return <Loader />;
+//     }
+//     if (status === STATUS.error) {
+//       return <p>{error}</p>;
+//     }
+
+//     return (
+//       <>
+//         <ul className={styles.ImageGallery}>
+//           {gallery.map(({ id, webformatURL, largeImageURL }) => {
+//             return (
+//               <ImageGalleryItem
+//                 key={id}
+//                 smallImg={webformatURL}
+//                 largeImg={largeImageURL}
+//                 handleOpenModal={this.props.handleOpenModal}
+//               />
+//             );
+//           })}
+//         </ul>
+//         {totalHits >= 12 * page && <Button onClick={this.handleLoader} />}
+//       </>
+//     );
+//   }
+// }
+
+// ImageGallery.propTypes = {
+//   handleOpenModal: PropTypes.func.isRequired,
+//   query: PropTypes.string.isRequired,
+// };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,36 +1,37 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useEffect } from 'react';
 import styles from './Modal.module.css';
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleButton);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleButton);
-  }
+export const Modal = ({ image, onClose }) => {
+  useEffect(() => {
+    const handleButton = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
 
-  handleButton = event => {
-    if (event.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-  handleBackdrop = event => {
+    window.addEventListener('keydown', handleButton);
+
+    return () => {
+      window.removeEventListener('keydown', handleButton);
+    };
+  });
+
+  const handleBackdrop = event => {
     if (event.target === event.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return (
-      <div className={styles.Overlay} onClick={this.handleBackdrop}>
-        <div className={styles.Modal}>
-          <img src={this.props.image} alt="img" />
-        </div>
+  return (
+    <div className={styles.Overlay} onClick={handleBackdrop}>
+      <div className={styles.Modal}>
+        <img src={image} alt="img" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Modal.propTypes = {
   image: PropTypes.string.isRequired,
